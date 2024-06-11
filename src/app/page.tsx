a5449a2021d886ac1341b691dca089b219fc7ea4
shegspear <shegspear@gmail.com>
2024-06-11 14:38:40 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,218 @@
-import Image from "next/image";
+"use client"
+
+import {useState, useEffect} from 'react';
 
 export default function Home() {
+  const [slots, setSlots] = useState<number>(15);
+
+  const [slotType, setSlotType] = useState<number>(0);
+
+  const [tier, setTeir] = useState<string>('');
+
+  const [space, setSpace] = useState<any>({});
+
+  const [hours, setHours] = useState<number>(0);
+
+  const [cost, setCost] = useState<number>(0);
+
+  const cal = (e:any) => {
+    e.preventDefault();
+    setHours(e.target.value);
+
+    if(slotType === 1) {
+      let amount = tier === 'basic' ? 10 : tier === 'premium' ? 15 : tier === 'executive' ? 20 : 0;
+      let cycle = e.target.value;
+      const sum = cycle < 3 ? (amount * e.target.value) : ((amount * e.target.value) / 10);
+      setCost(sum)
+    } else if (slotType === 2) {
+      let amount = 25;
+      let cycle = e.target.value;
+      const sum = cycle < 3 ? (amount * e.target.value) : ((amount * e.target.value) / 10);
+      setCost(sum);
+    }
+
+  }
+
+  const save = () => {
+    let arr:any = localStorage.getItem('bookings') || [];
+
+    let data = {
+      tier: `Booked tier ${tier}.`,
+      hour: `Booked session ${hours} hours.`,
+      cost: `Total cost ${cost} USD`
+    };
+
+    arr.push(data);
+
+    localStorage.setItem('bookings', JSON.stringify(arr));
+
+    setSlots( slots - 1)
+  }
+
+  useEffect(() => {
+    let arr:any = localStorage.getItem('bookings') || [];
+    setSlots(15 - arr.length);
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <h1
+        className='text-center text-white font-bold text-2xl mb-10'
+      >
+        Welcome To Book A Work space, we have {slots} slots available
+      </h1>
+
+      <div
+        className='w-10/12 mb-10'
+      >
+
+        <p
+          className='text-white font-semibold text-lg mb-10'
+        >
+          Which teir would you like ?
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        
+       <div
+        className='w-9/12 flex lg:flex-row flex-col items-center justify-between'
+       >
+          <div
+            className={`border-2 ${slotType === 1 ? 'border-green-300' : ''} rounded-lg p-4 cursor-pointer`}
+            onClick={() => setSlotType(1)}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <p
+              className='text-medium'
+            >
+              Individual Work Space
+            </p>
+          </div>
+
+          <div
+            className={`border-2 ${slotType === 2 ? 'border-green-300' : ''} rounded-lg p-4 cursor-pointer`}
+            onClick={() => setSlotType(2)}
+          >
+            <p
+              className='text-medium'
+            >
+            Team Work Space
+            </p>
+          </div>
+       </div>
+
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {
+        slotType !== 0 && (
+          <div
+            className='w-10/12 mb-10'
+          >
+            <p
+              className='text-medium mb-10'
+            >
+              Great you selected {slotType === 1 ? 'single work space' : "team work space"}.
+            </p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            {
+              slotType === 1 ? (
+                <div
+                className='w-9/12 flex lg:flex-row flex-col items-center justify-between'
+               >
+                  <div
+                    className={`border-2 ${tier === 'basic' ? 'border-green-300' : ''} rounded-lg p-4 cursor-pointer`}
+                    onClick={() => setTeir('basic')}
+                  >
+                    <p
+                      className='text-medium'
+                    >
+                      Basic
+                    </p>
+                  </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                  <div
+                    className={`border-2 ${tier === 'premium' ? 'border-green-300' : ''} rounded-lg p-4 cursor-pointer`}
+                    onClick={() => setTeir('premium')}
+                  >
+                    <p
+                      className='text-medium'
+                    >
+                      Premium
+                    </p>
+                  </div>
+        
+                  <div
+                    className={`border-2 ${tier === 'executive' ? 'border-green-300' : ''} rounded-lg p-4 cursor-pointer`}
+                    onClick={() => setTeir('executive')}
+                  >
+                    <p
+                      className='text-medium'
+                    >
+                    Executive
+                    </p>
+                  </div>
+               </div>
+              ) 
+              : slotType === 2 ? (
+                <div
+                className='w-9/12 flex lg:flex-row flex-col items-center justify-between'
+               >
+                <p
+                  className='text-medium mb-10'
+                >
+                  Save your slot.
+                </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                <div>
+                  <div className='mb-4'>
+                    <label>How many hours ?</label>
+                    <input type={'number'} max={24} className='ml-4 text-black' onChange={(e:any) => cal(e)} />
+                  </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                  <div className='mb-4'>
+                    <p>It would cost you {cost}</p>
+                    {hours > 3 && (<p>Great you got a 10% discount</p>)}
+                  </div>
+
+                  <button>
+                    Save
+                  </button>
+                </div>
+               </div>
+              ) : null
+            }
+          </div>
+        )
+      }
+
+      {
+        tier !== '' && (
+          <div
+            className='w-10/12'
+          >
+            <p
+              className='text-medium mb-10'
+            >
+              Save your slot.
+            </p>
+
+            <div>
+              <div className='mb-4'>
+                <label>How many hours ?</label>
+                <input type={'number'} max={24} className='ml-4 text-black' onChange={(e:any) => cal(e)} />
+              </div>
+
+              <div className='mb-4'>
+                <p>It would cost you {cost}.</p>
+
+                {hours > 3 && (<p>Great you got a 10% discount</p>)}
+              </div>
+
+              <button>
+                Save
+              </button>
+            </div>
+          </div>
+        )
+      }
+
     </main>
   );
 }
